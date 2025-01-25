15caa61a578024c61d6c390173e9a05d3d50814e
AayushG10 <aayushgoyal135@gmail.com>
2025-01-25 20:35:21 +0530

    Your commit messag

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+import { div } from "motion/react-client";
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Link href={"/"}>
+        <Menu setActive={setActive}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="HOME"
+          ></MenuItem>
+
+          <MenuItem setActive={setActive} active={active} item="COURSE">
+            <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+              <HoveredLink
+                title="React Master Kit"
+                href="https://reactmasterkit.com"
+                src="https://assets.aceternity.com/demos/reactmasterkit.webp"
+                description="Production ready React components for your next project" />
+              
+              
+            
+            </div>
+          </MenuItem>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="ROADMAP"
+          ></MenuItem>
+          {/* <Link href={"/contact"}> */}
+            <MenuItem
+
+              setActive={setActive}
+              active={active}
+              item="CONTACT US"
+            ></MenuItem>
+          {/* </Link> */}
+        </Menu>
+      </Link>
+    </div>
+  );
+}
+
+
+export default Navbar;
